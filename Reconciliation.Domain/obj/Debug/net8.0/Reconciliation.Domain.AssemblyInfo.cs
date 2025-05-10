726c8e17e352602bc9badb743960ccef144573c7
Christian Kinate <kinate.christian1989@yahoo.com>
2025-05-10 12:18:26 +0100

    Changes

--- Reconciliation.Domain/obj/Debug/net8.0/Reconciliation.Domain.AssemblyInfo.cs
+++ Reconciliation.Domain/obj/Debug/net8.0/Reconciliation.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reconciliation.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7855866b959c84ee1da0ad3a39fa2f1719ddce49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reconciliation.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reconciliation.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
